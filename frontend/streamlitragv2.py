62910b42cf7e3e5ad9030281fc47237d877e2c3f
IanPoe03 <158785787+IanPoe03@users.noreply.github.com>
2024-04-04 19:54:42 -0400

    Update streamlitragv2.py

--- frontend/streamlitragv2.py
+++ frontend/streamlitragv2.py
@@ -22,7 +22,7 @@ with st.sidebar:
 
 #If user enters an API key, load persistent directory and set up vector space from it
 if OPENAI_API_KEY:
-    persist_dir = 'rag-based-streamlit/tree/main/persistent_directory'
+    persist_dir = 'persistent_directory'
     embeddings =  OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
     vector_space = Chroma(persist_directory=persist_dir, embedding_function=embeddings)
 
